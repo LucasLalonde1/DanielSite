9ffb50e0fa9d6cf1744343c8ea8c027ddd13276d
LucasLalonde1 <lucaslalonde999@gmail.com>
2024-04-11 16:55:03 -0300

    fixed static root

--- danielPortfolio/settings.py
+++ danielPortfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/main/'
-STATIC_ROOT = '/home/dh_yk5fv5/danieldeganis.com/public/static/main/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
